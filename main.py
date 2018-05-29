156247f46ba0581942dee4e3f05adc6dc7cb10a0
Greg Attra <greg.attra@gmail.com>
2018-05-28 22:50:46 -0400

    style and denoise cost

--- main.py
+++ main.py
@@ -58,7 +58,37 @@ def create_content_loss(sess, model, content_image, layer_ids):
         for value, layer in zip(values, layers):
             value_const = tf.constant(value)
             loss = mean_squared_error(layer, value)
-            layers_losses.appen(loss)
+            layers_losses.append(loss)
         total_loss = tf.reduce_mean(layers_losses)
 
     return total_loss
+
+def gram_matrix(tensor):
+    shape = tensor.get_shape()
+    num_channels = int(shape[3])
+    matrix = tf.eshape(tensor, shape=[-1, num_channels])
+    gram = tf.matmul(tf.transpose(matrix), matrix)
+
+    return gram
+
+def create_style_loss(sess, model, style_image, layer_ids):
+    feed_dict = model.create_feed_dict(image=style_image)
+    layers = model.get_layer_tensors(layer_ids)
+    with model.graph.as_default():
+        gram_layers = [gram_matrix(layer) for layer in layers]
+        values = sess.run(gram_layers, feed_dict=feed_dict)
+        layer_losses = []
+        for value, gram_layer in zip(values, gram_layers):
+            value_const = tf.constant(value)
+            loss = mean_squared_error(gram_layer, value_const)
+            layers_losses.append(loss)
+
+        total_loss = reduce_mean(layers_losses)
+
+    return total_loss
+
+def create_denoise_loss(model):
+    loss = tf.reduce_sum(tf.abs(model.input[:, 1:, :, :] - model.input[:, :-1, :, :])) + \
+           tf.reduce_sum(tf.abs(model.input[:, :, 1:, :] - model.input[:, :, -1, :]))
+
+    return loss
